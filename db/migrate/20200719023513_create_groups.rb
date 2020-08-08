7588c084f8ea004c2eb85a94c4d8277f33c3a31c
riqui0130 <riquiffen8ru@icloud.com>
2020-08-08 11:14:22 +0900

    front1

--- db/migrate/20200719023513_create_groups.rb
+++ db/migrate/20200719023513_create_groups.rb
@@ -1,7 +1,6 @@
 class CreateGroups < ActiveRecord::Migration[6.0]
   def change
-    create_table :groups do |t|
-      t.string :name null: false unique: true
+    create_table :groups do |t|   
       t.timestamps
     end
   end
