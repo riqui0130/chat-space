94088288879f204eba98fd2a326ab92d7d9e4270
riqui0130 <riquiffen8ru@icloud.com>
2020-08-17 11:24:57 +0900

    edit

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
+<<<<<<< Updated upstream
   has_many :groups_users
   has_many :users, through: :groups_users
   has_many :messages
@@ -15,4 +16,9 @@ class Group < ApplicationRecord
       'まだメッセージはありません。'
     end
   end
+=======
+  has_many :group_users
+  has_many :users, through: :group_users
+  validates :name, presence: true, uniqueness: true
+>>>>>>> Stashed changes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  
   root "messages#index"
   resources :users, only: [:edit, :update]
+<<<<<<< Updated upstream
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
+=======
+  resources :groups, only: [:index, :new, :create, :edit, :update]
+>>>>>>> Stashed changes
 end
