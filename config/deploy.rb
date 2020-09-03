b1916dbd08585a9bb1fbee1ac53c95a3976db260
riqui0130 <riquiffen8ru@icloud.com>
2020-09-03 16:21:18 +0900

    1

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.14.1"
 
 set :application, "chat-space"
-set :repo_url, "git@github.com:riqui/chat-space.git"
+set :repo_url, "git@github.com:riqui0130/chat-space.git"
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
